07145b05587941cfd81020be211cec548aceecbf
lulu1403 <kulumfatma14@gmail.com>
2024-05-20 02:43:30 +0700

    beriDonasi page and tentang Kami page

--- slide.js
+++ slide.js
@@ -3,25 +3,19 @@ function showSlides() {
     let slideIndex = 0;
 
     setInterval(function() {
-        // Sembunyikan semua slide
         slides.forEach(function(slide) {
-            slide.style.display = 'none';
+            slide.classList.remove('active');
         });
 
-        // Tampilkan slide berikutnya
-        slides[slideIndex].style.display = 'block';
+        slides[slideIndex].classList.add('active');
 
-        // Perbarui indeks slide untuk slide berikutnya
         slideIndex++;
         if (slideIndex >= slides.length) {
-            slideIndex = 0; // Kembali ke slide pertama setelah mencapai akhir
+            slideIndex = 0; 
         }
-    }, 3500); // Ganti slide setiap 5 detik (5000 milidetik)
+    }, 3500); 
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Kode di dalam callback ini akan dijalankan saat DOMContentLoaded terjadi
-
-    // Panggil fungsi showSlides() atau kode JavaScript lainnya di sini
     showSlides();
 });
